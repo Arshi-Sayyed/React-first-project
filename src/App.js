cf09009c3ecd838745813c3a057e48e3a850ed84
Arshi-Sayyed <arshisayyed65@gmail.com>
2025-11-03 16:17:26 +0530

    Initial project upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Intro from "./Intro";
+
+function App() {
+  return (
+    <div>
+      <h1>React Project Setup Successful ✅</h1>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Intro.jsx
@@ -0,0 +1,10 @@
+function Intro() {
+  return (
+    <div>
+      <h2>Hello, React!</h2>
+      <p>This is my first React component — excited to start building!</p>
+    </div>
+  );
+}
+
+export default Intro;
